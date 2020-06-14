e69dd76f03bce896dad5bf4fd6fc34b11b403437
Krišs Ozoliņš <kriss@burti.lv>
2020-06-14 14:23:54 +0300

    ReactJs project and files for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+// Custom
+import './homepage.styless.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HATS</h1>
+          <span className='subtitle'>The Title</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>SNEAKERS</h1>
+          <span className='subtitle'>The Title</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>JACKETS</h1>
+          <span className='subtitle'>The Title</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>WOMENS</h1>
+          <span className='subtitle'>The Title</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>MENS</h1>
+          <span className='subtitle'>The Title</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage;
