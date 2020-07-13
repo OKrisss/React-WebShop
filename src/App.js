c49d7ecdd9ecb61138c3e03f2ec7ca6327828f2c
Krišs Ozoliņš <kriss@burti.lv>
2020-07-13 23:13:39 +0300

    react-router and Router navigation to hats HOC-higher order component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 
 // Custom
 import HomePage from './pages/homepage/homepage.component';
 
+const Hats = (props) => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={Hats} />
     </div>
   );
 }
